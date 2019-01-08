efcbe32be682174f8ec0fd580d9e58178af018b2
Nayem <nayem@Nayems-MacBook-Pro.local>
2019-01-08 10:41:11 +0600

    Initial Commit

---
+++ PagingVideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PagingVideoPlayer
+//
+//  Created by Nayem on 1/8/19.
+//  Copyright © 2019 Mufakkharul Islam Nayem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
